0984765308f8ad920aca842cdd912548f09470c8
knisslo4 <lkniss14@gmail.com>
2024-03-08 18:21:13 -0700

    added paychecks tab

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
+entries = []
+
 starting_balances = {
-    "Checking": 5542.10,
+    "Checking": 4006.55,
     "Savings": 0,
-    "Investing": 0,
+    "Invested": 2002.48,
     "Taxes": 355.77
 }
 
@@ -15,7 +17,13 @@ starting_balances = {
 def index():
     balances = starting_balances
     
-    return render_template('index_html', balances=balances)
+    return render_template('index.html', balances=balances)
+
+def paychecks():
+    one_month_ago = datetime.today() - timedelta(days=30)
+    one_month_from_date = datetime.today() + timedelta(days=30)
+    paycheck_entries = [entry for entry in entries if 'Paycheck' in entry['Description'] and one_month_ago <= entry['Date'] <= one_month_from_date]
+    return render_template('paychecks.html', paycheck_entries=paycheck_entries)
 
 if __name__ == '__main__':
     app.run(debug=True)
